62524e8263e72455f11e5ab0fb609d7e28af4c85
SALLY-4121 <salimah.yu@icloud.com>
2026-04-25 01:13:57 +0000

    Planner V1

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,35 +2,21 @@ import streamlit as st
 from openai import OpenAI
 
 client = OpenAI(api_key=st.secrets["api_key"])
-
-def get_standard_response(system_prompt, user_prompt):
-    """
-    Sends a prompt to the ChatGPT API where it will return a standard response.
-    ChatGPT will not remember any prior conversations.
-
-    Parameters:
-    - system_prompt (str): Directions on how ChatGPT should act.
-    - user_prompt (str): A prompt from the user.
-
-    Returns:
-    - (str): ChatGPT's response.
-    """
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": user_prompt}
-        ]
-    )
-    return response.choices[0].message.content
-
-with st.form("Language form"):
-    out_lang = st.selectbox("What languge do you want to output?",[
-        "English", "Spanish", "Chinese", "French", "German", "Italian", "Swedish"
-    ])
-    inp = st.text_input("Put your text to be translated here!")
+s = ("You are an organisation helper. You will be given a block of text and you must create tasks according to the priotities of the user, or in whichevery order you think is most suitible. You will create tasks with a title, and description for the user, with a star rating of 1 - 5 based on it's importance, 1 is least important, 5 is most.")
+   
+if "task" not in st.session_state:
+    st.session_state["task"]=[]
+if "chat" not in st.session_state:
+    st.session_state["chat"]=[{"role": "system", "content": s}]
+with st.form("Task form"):
+    user_input = st.text_area("Paste or type the tasks you wish to generate")
 
     press = st.form_submit_button("Click me to translate your text!")
 
     if press:
-        st.write(get_standard_response("You are a translater, the user will input text in any language. You will output it in the user's chosen output language.", "This is the language to translate to: " + out_lang + ". This is the user's inputed text: " + inp))
+        st.session_state['chat'].append({'role':'user','content':user_input})
+        response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=st.session_state["chat"]
+)
+        st.write(response.choices[0].message.content)
