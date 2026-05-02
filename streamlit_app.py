c88cfe94b1bd1a87172ace7a4ff8f9a4f3b7fa39
SALLY-4121 <salimah.yu@icloud.com>
2026-05-02 00:47:02 +0000

    Co-authored-by: jjjjhogan <jjjjhogan@users.noreply.github.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 from openai import OpenAI
+import json
 
 client = OpenAI(api_key=st.secrets["api_key"])
-s = ("You are an organisation helper. You will be given a block of text and you must create tasks according to the priotities of the user, or in whichevery order you think is most suitible. You will create tasks with a title, and description for the user, with a star rating of 1 - 5 based on it's importance, 1 is least important, 5 is most.")
+s = ("""You are an organisation helper. You will be given a block of text and you must create tasks according to the priotities of the user, 
+     or in whichevery order you think is most suitible. You will create tasks with a title, and description for the user, 
+     with a star rating of 1 - 5 based on it's importance, 1 is least important, 5 is most. Return only in JSON format with the following template
+     { 'title': {'desc':'description here', star:1}
+     }
+     """)
    
 if "task" not in st.session_state:
     st.session_state["task"]=[]
@@ -17,7 +23,8 @@ with st.form("Task form"):
         st.session_state['chat'].append({'role':'user','content':user_input})
         response = client.chat.completions.create(
     model="gpt-4o",
+    response_format={'type':'json_object'},
     messages=st.session_state["chat"]
 )
-        st.write(response.choices[0].message.content)
+        st.write(json.loads(response.choices[0].message.content))
         st.session_state['chat'].append({'role':'user','content':response.choices[0].message.content})
